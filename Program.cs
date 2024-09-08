faf9ad578958c6bcf63ad2aeb4f2a793b49b849b
Danielle Gouveia <danielle.vieira9@etec.sp.gov.br>
2024-09-08 19:04:28 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+const double ReceitaFarinha = 1;
+const double ReceitaAgua = 0.7;
+const double ReceitaFermento = 0.4;
+const double ReceitaSal = 0.02;
+
+double percentualPaoRelativoFarinha = ReceitaFarinha + ReceitaAgua + ReceitaFermento + ReceitaSal;
+
+double pesoPao, farinha, agua, fermento, sal;
+
+Console.WriteLine("--- Padaria ---\n");
+
+Console.WriteLine("Peso desejado de pão italiano (em gramas)...: ");
+pesoPao = Convert.ToDouble(Console.ReadLine());
+
+farinha = pesoPao / percentualPaoRelativoFarinha;
+
+agua = farinha * ReceitaAgua;
+fermento = farinha * ReceitaFermento;
+sal = farinha * ReceitaSal;
+
+Console.WriteLine("\nIngredientes:");
+
+Console.WriteLine($"Farinha....: {farinha:N0} g");
+Console.WriteLine($"Água.......: {agua:N0} g");
+Console.WriteLine($"Fermento...: {fermento:N0} g");
+Console.WriteLine($"Sal........: {sal:N0} g");
